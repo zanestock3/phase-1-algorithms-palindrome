2ef021b5eb0847efdd47b763352c0bb67424efd8
Zane Stock <zanestock3@gmail.com>
2022-05-02 06:33:20 -0500

    commit tree 0b0beec01bf964a829918350bc167b42f59834b4

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  if (word === word.split('').reverse().join('')) {
+    return true }
+    else {
+      return false
+    }
+  }
 
 /* 
   Add your pseudocode here
